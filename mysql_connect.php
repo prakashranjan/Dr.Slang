27dc2b3e9a5a1425fbb34ee4926c8b01739d5f03
Prakash Ranjan Singh <prakashranjansingh04@gmail.com>
2015-11-07 06:19:23 +0530

    Update mysql_connect.php

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,8 +1,8 @@
 <?php   
 $db_host = "localhost";
-$db_username = "1000253";
-$db_pass = "abdulkalam";
-$db_name = "1000253db2";
+$db_username = "*******";
+$db_pass = "**********";
+$db_name = "*********";
 
 @mysql_connect("$db_host","$db_username","$db_pass") or die ("could not connect");
 @mysql_select_db("$db_name") or die ("no database");
